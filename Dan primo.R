9a33f01f7ac99ace5f2d0097fbb3eb9f7bd699e6
lorenzo d'imporzano <73790665+LorenzodImporzano@users.noreply.github.com>
2021-03-08 12:32:37 +0100

    Add files via upload

---
+++ Dan primo.R
@@ -0,0 +1,38 @@
+install.packages("lmtest")
+
+rm(list=ls())
+library(OECD)
+library(ggplot2)
+library(lmtest)
+
+inflation_data=get_dataset("KEI", filter="CP+CPALTT01.DNK.GP.A", start_time=1983, end_time=(2019))
+unemployment_data=get_dataset("STLABOUR", filter="DNK.LRHUTTFE+LRHUTTTT.ST.A", start_time=1983, end_time=(2019))
+
+
+unemployment=unemployment_data$obsValue[39:74]
+inflation=inflation_data$obsValue[2:37]
+inflation_previous_period=inflation_data$obsValue[1:36]
+difference=inflation-inflation_previous_period
+summary(lm(difference ~ unemployment))
+e=ggplot(data=NULL,aes(x=unemployment,y=difference))
+e+geom_point()+geom_smooth(method=lm, se=FALSE)
+summary(lm(inflation ~ unemployment+inflation_previous_period))
+
+model <- lm(inflation ~ unemployment+inflation_previous_period)
+res <- resid(model)
+variance_hat = sum(res**2)/(length(res)-3)
+plot(model)
+
+
+# Heteroscedasticity tests:
+gqtest(model)
+bptest(model)
+
+# Error correlation tests:
+dwtest(model)
+
+# zero mean of residuals
+mean(res)
+
+# Test for linearity:
+reset(model)
