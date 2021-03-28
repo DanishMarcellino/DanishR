a94ce88fb98f84fc672c9ef866799ef9e1d5da7e
lorenzo d'imporzano <73790665+LorenzodImporzano@users.noreply.github.com>
2021-03-28 11:56:08 +0200

    Add files via upload

--- Work dan.R
+++ Work dan.R
@@ -14,10 +14,15 @@ inflation_data=get_dataset("KEI", filter="CP+CPALTT01.DNK.GP.A", start_time=1998
 unemployment_data=get_dataset("STLABOUR", filter="DNK.LRHUTTFE+LRHUTTTT.ST.A", start_time=1998, end_time=(2019))
 protection_data=get_dataset("EPL_CD", filter="DNK.EPC_V2", start_time=1998, end_time=(2019))
 wage_data=get_dataset("AV_AN_WAGE", filter="DNK.USDPPP", start_time=1998, end_time=(2019))
+#CPI_data=get_dataset("PRICES_CPI",filter="DNK.CPALTT01.GP.A",start_time=1998, end_time=(2019))
+#MEI_data=get_dataset("MEI_PRICES_PPI",filter="PIEAMP02+PIEAMP01+PIEATI02+PIEATI01+PIEAMI02+PIEAMI01+PIEAFD02+PIEAFD01+PIEAEN02+PIEAEN01+PITGVG02+PITGVG01+PITGIG02+PITGIG01+PITGCG02+PITGCG01+PITGCD02+PITGCD01+PITGND02+PITGND01+PISPPR02+PISPPR01+PISPIG02+PISPIG01+PISPFG02+PISPFG01+WPIATT01+WPIAMP01+WPOTFD01.DNK.GP+GY+IXOB.A",start_time=1998, end_time=(2019))
 
 #estimation of the mark-up
 real_wage=wage_data$obsValue[2:22]
-markup=1/real_wage-1 
+#CPI_real=CPI_data$obsValue[2:22]
+#MEI_real=MEI_data$obsValue[1238:1258]
+#markup=CPI_real/real_wage-1 
+markup=1/real_wage -1
 
 #extract relevant data
 protection=protection_data$obsValue[2:22]
@@ -49,7 +54,8 @@ plot3d$plane3d(model,lty.box="solid",col='blue')
 (res2=sum(res**2))
 (variance_hat=res2/(21-3))
 
-#then we plotted the fitted and the real values for inflation so to understand the goodness of the model
+#then we plotted the fitted and the real values for inflation so to understand the goodness of the model. 
+#We also plotted the residuals to have a even clearer idea of the goodness of the model
 y_hat <- fitted(model)
 plot(inflation, type='l',main="Actual,Fitted and Residuals",asp=1.2)
 lines(y_hat, col='green')
@@ -63,16 +69,12 @@ lines(res,col='orange')
 # Homoscedasticity tests:
 
 #The Goldfeld-Quandt test compares the variances of two submodels divided by a specified breakpoint 
-#and rejects if the variances differ. In our case we called the test on our model obtained through the 
-#linear regression and we got a p-value of 0.3736, meaning that the hypothesis that the variance is 
-#homoscedastic is not rejected with a very high confidence.
+#and rejects H0: homoscedasticity in favour of H1: vari=cXi^2 if the variances differ
 gqtest(model, order.by=u_difference)
 gqtest(model, order.by=inflation_previous_period)
 
-# The Studentized Breusch-Pagan test fits a linear regression model to the residuals of a linear regression 
+# The Breusch-Pagan test fits a linear regression model to the residuals of a linear regression 
 #model and rejects if too much of the variance is explained by the additional explanatory variables. 
-#So also this test was useful to understand whether or not homoscedasticity was respected also by the error term. 
-#In this case we got a p-value of 0.2126 so we can conclude that variances are indeed homoscedastic. 
 bptest(model, studentize=FALSE)
 
 
@@ -83,9 +85,14 @@ bptest(model, studentize=FALSE)
 (mean(res))
 
 # Error correlation tests:
+
+#The Breusch-Godfrey test tests H0: errors are uncorrelated against H1: errors are correlated up to degree m. 
+#To do this test a statistic with the residuals is built.
 bgtest(model,order=3)
 
-#test for normality
+#test for normality:
+
+#The Jarque-Bera tests test H0: errors are N(0,sigma^2) against H1: errors are not normal
 jarque.bera.test(res)
 
 
@@ -97,8 +104,6 @@ jarque.bera.test(res)
 #power in explaining the dependent variable, the model is mis-specified in the sense that the data generating 
 #process might be better approximated by a polynomial or another non-linear functional form.
 #If the null-hypothesis that all coefficients are zero is rejected, then the model suffers from misspecification.
-#In our case we get a p-value of 0.6536 so H0 is not rejected and therefore we can conclude that linearity
-#is plausible.
 reset(model)
 
 # Test for multicollinearity
